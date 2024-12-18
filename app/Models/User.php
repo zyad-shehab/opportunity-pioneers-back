53f2e64ea56cc0c42109bc50bbf8518481a70064
Houcem <houcem.fourati@ekwateur.fr>
2024-12-18 17:32:49 +0100

    initialise project

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,19 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, HasUuids;
-
-    protected $table = 'users';
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -24,16 +19,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'username',
         'email',
-        'phone',
-        'bio_ar',
-        'bio_en',
-        'profile_picture',
-        'type',
-        'status',
         'password',
-        'country_id',
     ];
 
     /**
@@ -44,8 +31,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-        'email_verified_code',
-        'phone_verified_code',
     ];
 
     /**
@@ -55,78 +40,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'phone_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
-    /**
-     * The attributes that should use uuid.
-     *
-     * @var array<int, string>
-     */
-    protected $uuids = ['id'];
-
-    /**
-     * Get the country that owns the user.
-     */
-    public function country()
-    {
-        return $this->belongsTo(Country::class);
-    }
-
-    /**
-     * Get the user's applications.
-     */
-    public function applications()
-    {
-        return $this->hasMany(Application::class);
-    }
-
-    /**
-     * Get the user's notifications.
-     */
-    public function notifications()
-    {
-        return $this->hasMany(Notification::class);
-    }
-
-    /**
-     * Get the user's jobs.
-     */
-    public function jobs()
-    {
-        return $this->hasMany(Job::class);
-    }
-
-    /**
-     * Get the user's services.
-     */
-    public function services()
-    {
-        return $this->hasMany(Service::class);
-    }
-
-    /**
-     * Get the user's projects.
-     */
-    public function projects()
-    {
-        return $this->hasMany(Project::class);
-    }
-
-    /**
-     * Get the user's donations.
-     */
-    public function donations()
-    {
-        return $this->hasMany(Donation::class);
-    }
-
-    /**
-     * Get the user's assignment.
-     */
-    public function assignments()
-    {
-        return $this->hasMany(Assignment::class);
-    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,40 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-
-        // Create countries table
-        Schema::create('countries', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('name_ar');
-            $table->string('name_en');
-            $table->string('code', 2)->unique();
-            $table->timestamps();
-            $table->softDeletes();
-        });
-
-        // Create users table
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->string('name');
-            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->string('phone')->uniqid();
-            $table->string('bio_ar')->nullable();
-            $table->string('bio_en')->nullable();
-            $table->string('profile_picture')->nullable();
-            $table->string('type');
-            $table->enum('status', ['active', 'inactive', 'suspended)'])->default('inactive');
-            $table->integer('email_verified_code')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->integer('phone_verified_code')->nullable();
-            $table->timestamp('phone_verified_at')->nullable();
             $table->string('password');
-            $table->uuid('country_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
-            $table->softDeletes();
-
-            $table->foreign('country_id')->references('id')->on('countries');
         });
     }
 
@@ -53,7 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('countries');
         Schema::dropIfExists('users');
     }
 };
