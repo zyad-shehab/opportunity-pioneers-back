b97b4234690f5d2bcc93bf773b07d2aa620fd8f5
AhmadAlketnani <ahmadalktnany383@gmail.com>
2024-12-28 11:57:43 +0200

    completed task:Role for job seeker

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('job-seeker')->group(function ()  {
+    Route::apiResource('roles', RoleController::class)
+    ->only(['index', 'store', 'destroy']);
+} );
