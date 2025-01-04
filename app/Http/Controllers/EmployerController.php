114465fe200bfac1cfdb7278d66bd93589a27b72
hamzaalbaghdady <hamzaalbaghdady971@gmail.com>
2025-01-04 13:19:53 +0200

    Make EmployerController methods: create,update, and show

--- app/Http/Controllers/EmployerController.php
+++ app/Http/Controllers/EmployerController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreEmployerRequest;
+use App\Http\Requests\UpdateEmployerRequest;
+use App\Http\Resources\EmployerResource;
+use App\Models\Employer;
 use Illuminate\Http\Request;
 
 class EmployerController extends Controller
 {
-    //
+    public function store(StoreEmployerRequest $request)
+    {
+        $validated = $request->validated();
+        $employer = Employer::create($validated);
+        return new EmployerResource($employer);
+    }
+    public function update(UpdateEmployerRequest $request, $id)
+    {
+        $validated = $request->validated();
+        $employer = Employer::findOrFail($id);
+        $employer->update($validated);
+        return new EmployerResource($employer);
+    }
+    public function show($id)
+    {
+        $employer = Employer::findOrFail($id);
+        return new EmployerResource($employer);
+    }
 }
