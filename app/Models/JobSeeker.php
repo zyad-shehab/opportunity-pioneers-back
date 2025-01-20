c12066752dc50a7c8911145343ed0ec8efbceb05
hamzaalbaghdady <hamzaalbaghdady971@gmail.com>
2025-01-20 23:18:04 +0200

    Implement JobSeeker_jobs migration and set up relations

--- app/Models/JobSeeker.php
+++ app/Models/JobSeeker.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class JobSeeker extends Model
 {
@@ -14,5 +15,12 @@ class JobSeeker extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function jobs(): BelongsToMany
+    {
+        return $this->BelongsToMany(Job::class, 'job_seeker_jobs')
+            ->withPivot('created_at')
+            ->withTimestamps();
     }
 }
