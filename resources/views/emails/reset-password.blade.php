bafee41fa337a1bcd55308afc0e57915e06833d3
MuntahaSh <mnthyshbat@gmail.com>
2025-01-21 20:05:13 +0200

    add view for email

---
+++ resources/views/emails/reset-password.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Reset Request</title>
+</head>
+<body>
+    <h1>Password Reset Request</h1>
+    <p>You requested to reset your password. Click the link below to proceed:</p>
+    <a href="{{ $resetUrl }}">{{ $resetUrl }}</a>
+    <p>Best regards</p>
+</body>
+</html>
