7c4233f087bb64f83f7ef121e480ab49424b7fdb
MuntahaSh <mnthyshbat@gmail.com>
2025-01-21 19:58:46 +0200

    modify migration , add id as primary key

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -6,16 +6,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->id();
+            $table->string('email');
+            $table->string('type');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
+            $table->unique(['email', 'type']);
         });
     }
 
